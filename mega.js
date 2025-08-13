397b0bf396fb91b2118bb76b5956ac06c09b54be
HASHAN-MD-BOT-IK <hashiyacoder70@gmail.com>
2025-08-13 08:48:53 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -12,7 +12,7 @@ import * as mega from 'megajs';
 
 // Mega authentication credentials
 const auth = {
-    email: 'Hashiboy2010@gmail.com', // Replace with your Mega email
+    email: 'becisoj767@edectus.com', // Replace with your Mega email
     password: 'Hsshss2024', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
